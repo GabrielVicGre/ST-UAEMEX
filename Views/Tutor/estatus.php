3dc4d1d8bf626a550641fe51fe1e5169f4cfe5f7
GabrielVicGre <gabovicgre@gmail.com>
2022-12-22 23:47:33 -0600

    Cambios vistas

--- Views/Tutor/estatus.php
+++ Views/Tutor/estatus.php
@@ -5,15 +5,14 @@ if (empty($_SESSION['usuario']) || $_SESSION['tipo_usuario'] != "Tutor") {
 } else {
     $_SESSION['seccion_menu'] = 'estatus';
 }
-
+/*
 require_once "../../Controllers/Tutor/estatusController.php";
-/*
-require_once "../../Controllers/Alumno/actividadesController.php";
-require_once "../../Controllers/Alumno/entregaController.php";
-require_once "../../Views/Graphics/PercentBar.php";*/
 
-$controller_estatus = new estatusController();
+require_once "../../Controllers/Alumno/actividadesController.php";
+require_once "../../Controllers/Alumno/entregaController.php";*/
+require_once "../../Views/Graphics/PercentBar.php";
 /*
+$controller_estatus = new estatusController();
 $controller_actividades = new actividadesController();
 $controller_entrega = new entregaController();
 */
@@ -40,75 +39,7 @@ $controller_entrega = new entregaController();
                 </div>
                 De click en el nombre de uno de sus tutorados para conocer las actividades que ha entregado.
                 
-                
-                <div class="p-5" id="accordion">
-                    <?php
-                    $alumnos = $controller_estatus->getAlumnos();
-                /*  $count = 0;
-                    foreach ($alumnos as $alumno) {
-                */
-                    ?>
-                    <!--
-                        <div class="card">
-                            <div class="card-header" id="heading<?php echo $count; ?>">
-                                <h5 class="mb-0">
-                                    <button class="btn container" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $count; ?>" aria-expanded="false" aria-controls="collapse<?php echo $count; ?>">
-                                        <div class=row>
-                                            <div class="col-3"><?php echo $alumno->no_cuenta; ?></div>
-                                            <div class="col-3"><?php echo $alumno->nombre; ?></div>
-                                            <div class="col-6">
-                                                <?php
-                                                $progress = new PercentBar($controller_actividades->getEntregaRate($alumno->id_alumno));
-                                                $progress->display();
-                                                ?>
-                                            </div>
-                                    </button>
-                                </h5>
-                            </div>
-                            <div id="collapse<?php echo $count; ?>" class="collapse" aria-labelledby="heading<?php echo $count; ?>">
-                                <div class="card-body">
-                                    <table class="table table-hover">
-                                        <?php
-                                        $actividades = $controller_actividades->getActividadesByAlumno($alumno->id_alumno);
-
-                                        foreach ($actividades as $actividad) {
-                                        ?>
-                                            <tr>
-                                                <td><?php echo $actividad->clave_actividad; ?></td>
-                                                <td class="text-truncate">
-                                                    <?php echo $actividad->desc_actividad; ?>
-                                                </td>
-                                                <td class="col-1">
-                                                    <?php
-                                                    $status = $controller_actividades->getStatus($actividad->id_actividad, $alumno->id_alumno);
-
-                                                    if ($status == true) {
-                                                    ?>
-                                                        <i class="bi bi-check-square"></i>
-                                                    <?php
-                                                    } else {
-                                                    ?>
-                                                        <i class="bi bi-square"></i>
-                                                    <?php
-                                                    }
-                                                    ?>
-                                                </td>
-                                            </tr>
-                                        <?php
-                                        }
-                                        ?>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    ->
-                    <?php
-                   /*     $count++;
-                    }*/
-                    ?>
-
-                </div>
-                
+           
 
 
                 <br><br><br>        
